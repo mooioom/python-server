8c9f3e582f890d912d0fd6a8d1322e0827adacdc
Eldad Eliyahu Levi <eldad_levi@hotmail.com>
2020-10-29 18:15:00 +0200

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-server
 
 Hello
+How are you?
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
+// testing the API call interface
+
 document.addEventListener('DOMContentLoaded',function(){
 
-    console.log('loaded')
+    console.log('loaded');
+
+    call('ping',function(r){
+        console.log('r',r);
+    },{
+        a : 1,
+        b : [
+            {
+                c : 1
+            }
+        ],
+        d : {
+            r : '123'
+        }
+    })
 
 })
